af733e4b541ebb2b8409443d03981a459bdd353c
Maggie Dreyer <mdreyer@lclark.edu>
2015-02-14 20:52:09 +0100

    Added basic UI functionality. No back-end logic yet

--- app/src/main/java/hu/ait/android/maggie/minesweeper/MainActivity.java
+++ app/src/main/java/hu/ait/android/maggie/minesweeper/MainActivity.java
@@ -1,9 +1,12 @@
 package hu.ait.android.maggie.minesweeper;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+
+import hu.ait.android.maggie.minesweeper.views.GameView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,5 +15,41 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mineBtn = (Button) findViewById(R.id.mineBtn);
+        Button expandBtn = (Button) findViewById(R.id.expandBtn);
+        final GameView gameBoard = (GameView) findViewById(R.id.board);
+
+        mineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Point selectedSquare = gameBoard.getSelectedSquare();
+                if (selectedSquare != null) {
+                    gameBoard.markMine(selectedSquare);
+                    //Add guess to game logic
+                }
+            }
+        });
+
+        expandBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Point selectedSquare = gameBoard.getSelectedSquare();
+                //If this square has already been expanded, do nothing!
+                if(selectedSquare != null) {
+                    gameBoard.showExpansion(new int[][]
+                           {{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+                            {0,1,0,1,2,3,0,0,0,0},
+                            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+                            {0,1,0,1,2,3,0,0,0,0},
+                            {0,1,0,1,2,3,0,0,0,0},
+                            {0,1,0,1,2,3,0,0,0,0},
+                            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+                            {0,1,0,1,2,3,0,0,0,0},
+                            {0,1,0,1,2,3,0,0,0,0},
+                            {0,1,0,1,2,3,0,0,0,0}});
+                }
+            }
+        });
     }
 }
--- app/src/main/java/hu/ait/android/maggie/minesweeper/views/GameView.java
+++ app/src/main/java/hu/ait/android/maggie/minesweeper/views/GameView.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.PointF;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,15 +17,25 @@ import java.util.List;
  */
 public class GameView extends View {
 
+    public static final int GRID_WIDTH = 10;
+
     private Paint paintBackground;
     private Paint paintGrid;
+    private Paint paintHighlight;
+    private Paint paintMine;
+    private Paint paintNoMine;
 
-    private List<PointF> touchCoords;
+    private Point selectedSquare;
+    private List<Point> mines;
+    private int[][] mineCounts;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        touchCoords = new ArrayList<>();
+        selectedSquare = null;
+
+        mines = new ArrayList<>();
+        mineCounts = new int[GRID_WIDTH][GRID_WIDTH];
 
         paintBackground = new Paint();
         paintBackground.setColor(Color.DKGRAY);
@@ -35,11 +45,25 @@ public class GameView extends View {
         paintGrid.setColor(Color.LTGRAY);
         paintGrid.setStyle(Paint.Style.STROKE);
         paintGrid.setStrokeWidth(3);
+
+        paintHighlight = new Paint();
+        paintHighlight.setColor(Color.RED);
+        paintHighlight.setStyle(Paint.Style.FILL);
+
+        paintMine = new Paint();
+        paintMine.setColor(Color.BLACK);
+        paintMine.setStyle(Paint.Style.FILL);
+        //paintMine.setTextSize(); Figure this out!
+
+        paintNoMine = new Paint();
+        paintNoMine.setColor(Color.WHITE);
+        paintNoMine.setStyle(Paint.Style.FILL);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        touchCoords.add(new PointF(event.getX(), event.getY()));
+        selectedSquare = getSquare(event.getX(), event.getY());
+        invalidate();
         return super.onTouchEvent(event);
     }
 
@@ -57,10 +81,61 @@ public class GameView extends View {
 
         canvas.drawRect(0, 0, getWidth(), getHeight(), paintBackground);
 
-        int interval = getWidth() / 10;
-        for(int i = 0; i < 11; i++){
+        int interval = getWidth() / GRID_WIDTH;
+        for (int i = 0; i < 11; i++) {
             canvas.drawLine(i * interval, 0, i * interval, getHeight(), paintGrid);
-            canvas.drawLine(0, i * interval, getWidth(),i * interval, paintGrid);
+            canvas.drawLine(0, i * interval, getWidth(), i * interval, paintGrid);
         }
+
+        //Highlight selected square
+        if (selectedSquare != null) {
+            canvas.drawRect(selectedSquare.x * interval + 2, selectedSquare.y * interval + 2,
+                    (selectedSquare.x + 1) * interval - 1, (selectedSquare.y + 1) * interval - 1,
+                    paintHighlight);
+        }
+
+        for (Point point : mines) {
+            canvas.drawCircle(point.x * interval + (interval / 2) + 1,
+                              point.y * interval + (interval / 2) + 1,
+                              interval / 3,
+                              paintMine);
+        }
+
+        for (int i = 0; i < GRID_WIDTH; i++) {
+            for (int j = 0; j < GRID_WIDTH; j++) {
+                if (mineCounts[i][j] >= 0) {
+                    canvas.drawRect(j * interval + 2, i * interval + 2,
+                            (j + 1) * interval - 1, (i + 1) * interval - 1,
+                            paintNoMine);
+                }
+                if (mineCounts[i][j] > 0) {
+                    canvas.drawText(Integer.toString(mineCounts[i][j]), j * interval + 19,
+                            i * interval + 27, paintMine);
+                }
+            }
+        }
+    }
+
+    private Point getSquare(float xIn, float yIn) {
+        int interval = getWidth() / GRID_WIDTH;
+        int x = (int) (xIn / interval);
+        int y = (int) (yIn / interval);
+        return new Point(x, y);
+    }
+
+    public Point getSelectedSquare() {
+        return selectedSquare;
+    }
+
+    public void markMine(Point square) {
+        mines.add(square);
+        selectedSquare = null;
+        invalidate();
+    }
+
+    public void showExpansion(int[][] newMineCounts) {
+        mineCounts = newMineCounts;
+        selectedSquare = null;
+        invalidate();
     }
 }
